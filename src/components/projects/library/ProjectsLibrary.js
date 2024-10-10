88a32c77421c479086e8047f09c4fb61b9b6a796
Outfoxeed <letourneaumorigan@gmail.com>
2024-10-10 23:24:59 +0200

    [UPDATE] ProjectsLibrary displaying all projects in the json

--- src/components/projects/library/ProjectsLibrary.js
+++ src/components/projects/library/ProjectsLibrary.js
@@ -4,25 +4,16 @@ import ProjectTile from '../tiles/ProjectTile';
 import Title from '../../generics/title/Title';
 
 export default function ProjectsLibrary({ projectsData }) {
-  const projectToShow = [
-    projectsData.justdance,
-    projectsData.kanpeki,
-    projectsData.alinco,
-  ];
-
   return (
     <>
         <div className="project-library">
           <Title>My Projects</Title>
           <div className="projects-container">
-            { projectToShow &&
-              projectToShow.map((x) => {
-                return (
-                  <ProjectTile 
-                    projectData={x}/>
-                )
-              })
-            }
+            { projectsData &&
+              Object.entries(projectsData).map(([key, value]) => (
+                <ProjectTile 
+                    projectData={value}/>
+            ))}
           </div>
         </div>
     </>
